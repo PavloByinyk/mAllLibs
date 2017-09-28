91565651d8c0c1b7cf7db6524de286df66c9944c
pavlo <pbyinyk@gmail.com>
2017-09-28 21:57:20 +0300

    compile ButterKnife, add mainLayout, all works"

--- app/src/main/java/com/example/gottgried/musealllibs/MainActivity.java
+++ app/src/main/java/com/example/gottgried/musealllibs/MainActivity.java
@@ -10,46 +10,76 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.gottgried.musealllibs.models.Dog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = MainActivity.class.getName();
+    private Realm realmDb;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    @BindView(R.id.et_dog_name)   EditText etDogName;
+    @BindView(R.id.et_dog_mark)   EditText etDogMark;
+    @BindView(R.id.et_dog_age)   EditText etDogAge;
+    @BindView(R.id.btn_add_dog)   Button btnAddDog;
+    @BindView(R.id.btn_delete_dog)   Button btnDeleteDog;
+    @BindView(R.id.btn_show_all_dogs)   Button btnShowAllDogs;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    @OnClick(R.id.btn_add_dog)
+    public void addDogToDb(final View view){
+        realmDb.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+                Dog dog = bgRealm.createObject(Dog.class);
+                dog.setName(etDogName.getText().toString());
+                dog.setMark(etDogMark.getText().toString());
+                dog.setAge(Integer.valueOf(etDogAge.getText().toString()));
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                Snackbar.make(view, "the dog was added succesfully", Snackbar.LENGTH_SHORT).setAction("Action" , null).show();
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                Log.d(LOG_TAG, "onError: ***" + error.getMessage().toString());
+            }
+        });
+
+    }
+
+    @OnClick(R.id.btn_delete_dog)
+    public void deleteDogFromDb(View view){}
+
+    @OnClick(R.id.btn_show_all_dogs)
+    public void shoAllDogs(View view){}
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
+        realmDb = Realm.getDefaultInstance();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,98 +92,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
+    protected void onDestroy() {
+        super.onDestroy();
+        realmDb.close();
     }
 }
--- app/src/main/java/com/example/gottgried/musealllibs/models/Dog.java
+++ app/src/main/java/com/example/gottgried/musealllibs/models/Dog.java
@@ -1,8 +1,40 @@
 package com.example.gottgried.musealllibs.models;
 
+import io.realm.RealmModel;
+import io.realm.RealmObject;
+
 /**
  * Created by Gottgried on 28.09.2017.
  */
 
-public class Dog  {
+public class Dog extends RealmObject {
+
+    private String name;
+    private String mark;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
